43157e6d5ef7ca5356efdbe4ae63cf756fd2f5ef
reizist <reizist@gmail.com>
2014-06-27 15:37:03 +0900

    implement recipe to install zsh

--- site-cookbooks/zsh/recipes/default.rb
+++ site-cookbooks/zsh/recipes/default.rb
@@ -2,7 +2,11 @@
 # Cookbook Name:: zsh
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Copyright 2014, reizist
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package 'zsh' do
+  action :install
+end
